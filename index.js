261169671edb54c165a34474da385c4d03d756b0
Neo Zhang <zhangz73@cs.washington.edu>
2023-02-19 17:32:16 -0500

    debug js

--- index.js
+++ index.js
@@ -37,7 +37,9 @@
     show("home");
     for(var i = 0; i < categories.length; i++){
       var str = "menu-" + categories[i];
-      c(str).onclick = function(){load("sec-" + categories[i])};
+      //c(str).onclick = function(){load("sec-" + categories[i])};
+      var target = "sec" + categories[i];
+      c(str).getElementsByTagName("a")[0].href = "javascript: (function(){load(\"" + target + "\")})();";
     }
   };
 })();
