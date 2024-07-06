92ba76cc21d10c653fb271ce547091c7302ab253
Neo Zhang <zhangz73@cs.washington.edu>
2024-07-06 15:58:14 -0700

    separate cv/resume sections, add leadership activities

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 /*global fetch*/
 (function(){
   let curr = null;
-  let categories = ["home", "publications", "teaching", "industry", "cv",
+  let categories = ["home", "publications", "teaching", "industry", "leadership", "cv",
                     "contact"];
   function load(next){
     hide(curr);
     show(next);
+    updateCVList("contract");
   }
-  function show(name){
-    curr = name;
+  function show(name, updatePtr = true){
+    if(updatePtr){
+      curr = name;
+    }
     let list = qsa("." + name);
     for(let i = 0; i < list.length; i++){
       list[i].classList.remove("hidden");
@@ -29,6 +32,22 @@
   function qsa(selector){
     return document.querySelectorAll(selector);
   }
+  function swapCV(symb){
+    if(symb == "short"){
+      $("cv-view").classList.add("hidden");
+      $("resume-view").classList.remove("hidden");
+    } else{
+      $("resume-view").classList.add("hidden");
+      $("cv-view").classList.remove("hidden");
+    }
+  }
+  function updateCVList(symb){
+    if(symb == "expand"){
+      show("dropdown-content-cv", false);
+    } else{
+      hide("dropdown-content-cv");
+    }
+  }
   window.onload = function(){
     curr = "sec-home";
     for(let i = 0; i < categories.length; i++){
@@ -36,9 +55,15 @@
     }
     show("sec-home");
     for(let i = 0; i < categories.length; i++){
-      let str = "menu-" + categories[i];
-      let target = "sec-" + categories[i];
-      c(str).getElementsByTagName("a")[0].onclick = function(){load(target)};
+      if(categories[i] != "cv"){
+        let str = "menu-" + categories[i];
+        let target = "sec-" + categories[i];
+        c(str).getElementsByTagName("a")[0].onclick = function(){load(target)};
+      }
     }
+    updateCVList("contract");
+    $("menu-item-cv").onmouseover = function(){updateCVList("expand")};
+    $("cv-long-link").onclick = function(){load("sec-cv"); swapCV("long")};
+    $("cv-short-link").onclick = function(){load("sec-cv"); swapCV("short")};
   };
 })();
